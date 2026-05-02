b91a210a9c6d60d6dc759340eb40155be4d6bc91
nate <271378545+productionsaiinc-lgtm@users.noreply.github.com>
2026-05-02 03:54:48 -0400

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -10,6 +10,10 @@ export default function Home() {
 
   const features = [
     {
+     title:"New Features"
+     description: "MDM Mobile Device Management, License plate lookup/vin, Cloud storage and sync, virtual phones,Computers, Canary tokens, Temporary Emails, Pentest Labs, Url Shortener,Sim/phone Querys,Imei and CreditCard checks,password cracker, deepfake search, VPN services and much more."
+    },
+    {
       icon: Radar,
       title: "Network Scanner",
       description: "Advanced port scanning and network reconnaissance",
