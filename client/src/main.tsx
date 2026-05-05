2a974ffab199fcfcfb0c296121aeaaf8f5d36494
nate <271378545+productionsaiinc-lgtm@users.noreply.github.com>
2026-05-04 23:15:42 -0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,12 @@
+import { createRoot } from "react-dom/client";
 import { trpc } from "@/lib/trpc";
 import { UNAUTHED_ERR_MSG } from "@shared/const";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink, TRPCClientError } from "@trpc/client";
-import { createRoot } from "react-dom/client";
 import superjson from "superjson";
 import App from "./App";
-import { getLoginUrl } from "./const";
 import "./index.css";
+import { getLoginUrl } from "./const";
 
 const queryClient = new QueryClient();
 
