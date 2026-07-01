57e8ed019ca16947a95d9ee26420fd434a3ab55b
nate <271378545+productionsaiinc-lgtm@users.noreply.github.com>
2026-07-01 07:17:35 -0400

    Add TRPC client setup in trpc.ts

---
+++ src/lib/trpc.ts
@@ -0,0 +1,13 @@
+import { createTRPCReact } from '@trpc/react-query'
+import { TRPC_URL } from './api-config'
+
+export const trpc = createTRPCReact<AppRouter>()
+
+export const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: TRPC_URL,
+      credentials: 'include', // Include cookies for auth
+    }),
+  ],
+})
